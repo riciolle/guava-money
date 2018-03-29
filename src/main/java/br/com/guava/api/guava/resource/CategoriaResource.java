22f414239c06c22a4de40ff71694c661a70c3f41
Leonardo Riciolle <leonardo.aires@centralit.com.br>
2018-03-29 14:05:09 -0300

    Melhoria e Continuação do estudo

--- src/main/java/br/com/guava/api/guava/resource/CategoriaResource.java
+++ src/main/java/br/com/guava/api/guava/resource/CategoriaResource.java
@@ -1,17 +1,17 @@
 package br.com.guava.api.guava.resource;
 
+import java.net.URI;
 import java.util.List;
 
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.guava.api.guava.entity.Categoria;
 import br.com.guava.api.guava.repository.CategoriaRepository;
@@ -24,10 +24,22 @@ public class CategoriaResource {
 	private CategoriaRepository categoriaRepository;
 
 	@PostMapping
-	@ResponseStatus(HttpStatus.CREATED)
-	public void save(@RequestBody Categoria categoria, HttpServletResponse response) {
-		categoriaRepository.save(categoria);
-		Serv
+	public ResponseEntity<Categoria> save(@RequestBody Categoria categoria) {
+		Categoria categoriaSaved = categoriaRepository.save(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/codigo").buildAndExpand(categoriaSaved.getCodigo()).toUri();
+//		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(categoriaSaved);
+	}
+	
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Categoria> getByCodigo(@PathVariable Long codigo) {
+		Categoria categoria = categoriaRepository.findOne(codigo);
+		if (categoria != null) {
+			return ResponseEntity.ok(categoria);
+		} else {
+			return ResponseEntity.notFound().build();
+		}
 	}
 	
 	@GetMapping
