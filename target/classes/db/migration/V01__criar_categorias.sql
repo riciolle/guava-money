1000a17de1ab9aeebaa2df78c96a17b99bd1a485
leonardo.aires <leonardo.aires@centralit.com.br>
2018-12-13 14:18:24 -0200

    Alterações que ficaram sem ser comitadas

---
+++ target/classes/db/migration/V01__criar_categorias.sql
@@ -0,0 +1,5 @@
+CREATE TABLE categoria(
+	codigo SERIAL,
+	nome VARCHAR(64) NOT NULL,
+ CONSTRAINT pk_codigo_categoria PRIMARY KEY (codigo)
+);
---
+++ target/classes/db/migration/V02__criar_pessoa.sql
@@ -0,0 +1,13 @@
+CREATE TABLE pessoa (
+	codigo SERIAL,
+	nome VARCHAR(64) NOT NULL,
+	logradouro VARCHAR(64),
+	numero VARCHAR(64),
+	complemento VARCHAR(64),
+	bairro VARCHAR(64),
+	cep VARCHAR(64),
+	cidade VARCHAR(64),
+	estado VARCHAR(64),
+	ativo BOOLEAN NOT NULL,
+CONSTRAINT pk_codigo_pessoa PRIMARY KEY (codigo)
+);
